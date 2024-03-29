dec97de1e11bdcf60fd1f90b875641bde97a355f
Mohamednathim <mohamednathim84@gmail.com>
2024-03-29 18:17:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "./components/AppBar";
+import Filter from "./components/Filter";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+import SeasonSpecials from "./components/SeasonSpecials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar />
+      <Filter />
+      <Items />
+      <SeasonSpecials />
+      <Footer />
+    </>
   );
 }
 
